30d6d78d6dd81861bc6f9e8006d2b1b64a82874a
alemir0 <miroshnikov_04@bk.ru>
2022-10-06 15:00:14 +0300

    noviy

--- DateTime.pas
+++ DateTime.pas
@@ -2,4 +2,9 @@
   var x:=DateTime.Now.Year;
   println('Високосный год', ( (x mod 4=0) and (x mod 100<>0)) or (x mod 400=0));
   
+  var (a,s):=(random(1,31),random(1.31));
+  if a>s then
+    print(a)
+  else print(s);
+  
 end.
